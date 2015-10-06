7ef05635a818c496e20b4909f779298b140bf17f
Benjamin Röhrl <b.roehrl@gmail.com>
2015-10-06 19:05:33 +0200

    small fix

--- assets/js/history.js
+++ assets/js/history.js
@@ -1,4 +1,4 @@
 document.forms["newsletterForm"].onsubmit = function(){
-    this.innerHTML = <p>Erfolgreich übertragen!<p> + this.innerHTML;
+    this.innerHTML = "<p>Erfolgreich übertragen!</p>" + this.innerHTML;
     return false;
 }
