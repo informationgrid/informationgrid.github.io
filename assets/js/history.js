3c528c9f8ffeb26f2492f5b1767c34006176c112
Benjamin Röhrl <b.roehrl@gmail.com>
2015-10-07 10:58:01 +0200

    added success text after submitting newsletter request

--- assets/js/history.js
+++ assets/js/history.js
@@ -1,4 +1,26 @@
-document.forms["newsletterForm"].onsubmit = function(){
-    this.innerHTML = "<p>Erfolgreich übertragen!</p>" + this.innerHTML;
-    return false;
+window.onload = function() {
+  var subOrUnsub = window.location.search;
+  var subForm = document.forms["newsletterForm"];
+
+  switch (subOrUnsub) {
+    case "subscribe":
+      subForm.innerHTML += "<p><b>Erfolgreich angemeldet!</b></p>";
+      break;
+    case "unsubscribe":
+      subForm.innerHTML += "<p><b>Erfolgreich abgemeldet!</b></p>";
+      break;
+  }
+
+};
+
+document.forms["newsletterForm"].onsubmit = function() {
+  this.innerHTML = "<p><b>Erfolgreich übertragen!</b></p>" + this.innerHTML;
+  var unsub = document.getElementById('unsubscribe').checked;
+  var nextURL = document.getElementById("nextURL");
+
+  if (unsub == "true") {
+    nextURL.value += "?unsubscribe";
+  } else {
+    nextURL.value += "?subscribe";
+  }
 }
